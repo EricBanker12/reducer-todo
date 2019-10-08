32d1588c13e29fd409ac28ba27cd097fd1fbb8c9
ericbanker12 <ericbanker12@gmail.com>
2019-10-08 14:36:29 -0500

    create reducer

---
+++ app/src/reducers/reducer.js
@@ -0,0 +1,21 @@
+const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Another item',
+        completed: false,
+        id: 3892987590
+    },
+]
+
+function reducer(action) {
+    switch (action.type) {
+        default:
+            return //state
+    }
+}
+
+export default {reducer, initialState}
